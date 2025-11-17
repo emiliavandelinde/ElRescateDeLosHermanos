83ed8c6b26e61cad933379c707bd129fe61d90d6
Emilia <emiliavandelinde@gmail.com>
2025-11-17 13:41:55 -0300

    Actualizacion, mejoras, nuevos assets y nivel final

--- suelo.tsx
+++ suelo.tsx
@@ -1,4 +1,84 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="suelo" tilewidth="32" tileheight="32" tilecount="16" columns="4">
  <image source="suelo.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup>
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="suelo" x="0" y="0" width="31" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
